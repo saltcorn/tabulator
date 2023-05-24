3b8c788bd81e963d5b117705df6b6506966713ab
Tom Nielsen <tanielsen@gmail.com>
2023-05-24 11:31:28 +0100

    disable popup in calc row

--- public/custom.js
+++ public/custom.js
@@ -312,6 +312,7 @@ function pivotEditCheck(cell) {
 function pivot_edit_popup(e, cell) {
   if (!window.pivot_tabulator_edit_view) return;
   const data = cell.getRow().getData();
+  if (data.disableEdit) return;
   const field = cell.getField();
   const id = data.ids[field];
 
